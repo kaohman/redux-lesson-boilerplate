dba3fef73c12c92d22f1e4e9ba54baa590e18737
Will Mitchell <wvmitchell@gmail.com>
2018-05-15 16:21:52 -0600

    move component into container file

--- src/containers/AddTodoFormContainer.js
+++ src/containers/AddTodoFormContainer.js
@@ -1,6 +1,31 @@
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
-import AddTodoForm from '../components/AddTodoForm'
+
+class AddTodoForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { text: '' }
+  }
+
+  render() {
+    console.log(this.props);
+    const { handleSubmit, todos } = this.props;
+    return (
+      <section>
+        <form onSubmit={  (e) => {
+              e.preventDefault()
+              handleSubmit(this.state.text, todos.length)
+        }}>
+          <input  value={this.state.text}
+                  placeholder="Add A Todo"
+                  onChange={(e) => this.setState({ text: e.target.value} )} />
+          <button>Add Todo</button>
+        </form>
+      </section>
+    )
+  }
+}
 
 const mapStateToProps = (state) => {
   console.log(state)
